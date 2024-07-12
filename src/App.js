c3ab1e497a75f40e8e8e0651a91e558e5ca5f1f8
송연근 <daddy73e@mobmail.shinhan.com>
2024-07-12 09:31:48 +0900

    CRUD버튼 추가

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function createDatabase(key) {
+    console.log(`create Database, ${key}`);
+  }
+
+  function readDatabase(key) {
+    console.log(`read Database, ${key}`);
+  }
+
+  function updateDatabase(key) {
+    console.log(`update Database, ${key}`);
+  }
+
+  function deleteDatabase(key) {
+    console.log(`delete Database, ${key}`);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div className='button-container'>
+          <button onClick={() => createDatabase("John")}>Create</button>
+          <button onClick={() => readDatabase("John")}>Read</button>
+          <button onClick={() => updateDatabase("John")}>Update</button>
+          <button onClick={() => deleteDatabase("John")}>Delete</button>
+        </div>
       </header>
     </div>
   );
