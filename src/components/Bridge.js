5b0b07ee7b0c89488412e845eeae464a649e150d
송연근 <daddy73e@mobmail.shinhan.com>
2024-09-10 14:27:03 +0900

    브릿지 버튼 grid추가

--- src/components/Bridge.js
+++ src/components/Bridge.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import '../App.css';
+
 
 const Bridge = () => {
+  const buttonsSection1 = Array.from({ length: 10 }, (_, i) => `Button ${i + 1}`);
+  const buttonsSection2 = Array.from({ length: 5 }, (_, i) => `Button ${i + 1}`);
+
   return (
     <div>
       <div>브릿지 테스트 페이지 입니다.</div>
-    </div>
+    <section className="section">
+      <h2>Section 1</h2>
+      <div className="grid-container">
+        {buttonsSection1.map((buttonText, index) => (
+          <button key={index} className="grid-item">
+            {buttonText}
+          </button>
+        ))}
+      </div>
+    </section>
+
+    <section className="section">
+      <h2>Section 2</h2>
+      <div className="grid-container">
+        {buttonsSection2.map((buttonText, index) => (
+          <button key={index} className="grid-item">
+            {buttonText}
+          </button>
+        ))}
+      </div>
+    </section>
+  </div>
   );
 };
 
