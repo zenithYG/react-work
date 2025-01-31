367fae53c9ee039ef8fde0929820308b2fd7fdc4
송연근 <daddy73e@mobmail.shinhan.com>
2025-01-31 14:09:47 +0900

    첫 페이지 signin

--- src/App.js
+++ src/App.js
@@ -49,8 +49,8 @@ const App = () => {
           <Route path="/admin" element={<Admin />} />
           <Route path="/bridge" element={<Bridge />} />
           <Route path="/teamCraft" element={<TeamCraft />} />
-          <Route path="/" element={<Navigate to="/tokenLogin" />} />
-          <Route path="*" element={<Navigate to="/tokenLogin" />} />
+          <Route path="/" element={<Navigate to="/signin" />} />
+          <Route path="*" element={<Navigate to="/signin" />} />
         </Routes>
       </Container>
     </Router>
