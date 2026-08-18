cc6ec0a5c356e13b80d4ba0eaad81a3dca3c0e60
zenithYG <daddy73e@gmail.com>
2026-08-18 13:08:51 +0900

    Update Millie logo image

--- src/constants/company.js
+++ src/constants/company.js
@@ -3,7 +3,7 @@ export const COMPANY = {
     NAME: "밀리의 서재",
     SUPPORT_DEPARTMENT: "iOS개발",
     COLORS: {
-        PRIMARY: "#FFEB60",
+        PRIMARY: "#FFF200",
         PRIMARY_FONT: "#000000"
     },
 };
